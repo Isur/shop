f00eccdf9f8ce4a2ebd105c33e4e18525c1da78c
Isur <bednarczykartur96@gmail.com>
2018-08-06 12:15:30 +0200

    connection with ldap

--- server.js
+++ server.js
@@ -5,6 +5,35 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
+// ldap 
+const ldap = require('ldapjs');
+const client = ldap.createClient({
+  url: 'ldap://localhost:389'
+});
+
+var opts = {
+  filter: '(&(l=Seattle)(email=*@foo.com))',
+  scope: 'sub',
+  attributes: ['dn', 'sn', 'cn']
+};
+
+client.search('o=example', opts, function(err, res) {
+  res.on('searchEntry', function(entry) {
+    console.log('entry: ' + JSON.stringify(entry.object));
+  });
+  res.on('searchReference', function(referral) {
+    console.log('referral: ' + referral.uris.join());
+  });
+  res.on('error', function(err) {
+    console.error('error: ' + err.message);
+  });
+  res.on('end', function(result) {
+    console.log('status: ' + result.status);
+  });
+});
+
+// 
+
 const jwt = require('jsonwebtoken');
 const exjwt = require('express-jwt');
 
